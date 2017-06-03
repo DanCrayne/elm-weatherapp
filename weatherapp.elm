be8a830e12d5924f001af44abd11454e7853540f
dcrayne <dan.crayne@gmail.com>
2017-06-03 16:26:57 -0700

    establish parsing of json nested field

--- weatherapp.elm
+++ weatherapp.elm
@@ -2,13 +2,13 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http
-import Json.Decode as Decode
+import Json.Decode exposing (..)
 
 
 
 main =
   Html.program
-    { init = init "97045"
+    { init = init "97202"
     , view = view
     , update = update
     , subscriptions = subscriptions
@@ -49,8 +49,14 @@ update msg model =
       (model, getWeather model.zipCode)
 
     NewWeatherData (Ok newWeatherData) ->
-      (Model model.zipCode "" "" "" newWeatherData, Cmd.none)
---      (Model model.weatherData newWeatherData, Cmd.none)
+      case decodeJson of
+        Err msg ->
+          (Model model.zipCode "" "" "" (newWeatherData ++ msg), Cmd.none)
+           
+        Ok value ->
+          (Model model.zipCode "" "" "" (newWeatherData ++ toString value), Cmd.none)
+
+--      (Model model.zipCode "" "" "" newWeatherData, Cmd.none)
 
     NewWeatherData (Err _) ->
       (Model model.zipCode "" "" "" "Error while sending", Cmd.none)
@@ -87,7 +93,7 @@ getWeather : String -> Cmd Msg
 getWeather zipCode = 
   let weatherRequest = 
     { method = "GET"
-    , headers = [Http.header "token" "<some token>"]
+    , headers = [Http.header "token" "hntEzDOlCPVILyHVyUIAzvQkvPbrkEBG"]
     , url = "https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&locationid=ZIP:28801&startdate=2010-05-01&enddate=2010-05-01"
     , body = Http.emptyBody
     , expect = Http.expectString
@@ -97,3 +103,80 @@ getWeather zipCode =
     }
   in
     Http.send NewWeatherData (Http.request weatherRequest)
+
+
+decodeJson : Result String Int
+decodeJson = decodeString (field "metadata" (field "resultset" (field "offset" int))) json
+
+
+json : String
+json = """
+{
+  "metadata": {
+    "resultset": {
+      "offset": 1,
+      "count": 8,
+      "limit": 25
+    }
+  },
+
+  "results": [
+    {
+      "date": "2010-05-01T00:00:00",
+      "datatype": "PRCP",
+      "station": "GHCND:US1NCBC0005",
+      "attributes": ",,N,",
+      "value": 0
+    },
+    {
+      "date": "2010-05-01T00:00:00",
+      "datatype": "SNOW",
+      "station": "GHCND:US1NCBC0005",
+      "attributes": ",,N,",
+      "value": 0
+    },
+    {
+      "date": "2010-05-01T00:00:00",
+      "datatype": "PRCP",
+      "station": "GHCND:USW00013872",
+      "attributes": ",,0,2400",
+      "value": 3
+    },
+    {
+      "date": "2010-05-01T00:00:00",
+      "datatype": "SNOW",
+      "station": "GHCND:USW00013872",
+      "attributes": ",,0,",
+      "value": 0
+    },
+    {
+      "date": "2010-05-01T00:00:00",
+      "datatype": "SNWD",
+      "station": "GHCND:USW00013872",
+      "attributes": ",,0,",
+      "value": 0
+    },
+    {
+      "date": "2010-05-01T00:00:00",
+      "datatype": "TMAX",
+      "station": "GHCND:USW00013872",
+      "attributes": ",,0,2400",
+      "value": 267
+    },
+    {
+      "date": "2010-05-01T00:00:00",
+      "datatype": "TMIN",
+      "station": "GHCND:USW00013872",
+      "attributes": ",,0,2400",
+      "value": 139
+    },
+    {
+      "date": "2010-05-01T00:00:00",
+      "datatype": "TOBS",
+      "station": "GHCND:USW00013872",
+      "attributes": ",,0,2400",
+      "value": 206
+    }
+  ]
+}
+"""
