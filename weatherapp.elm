8dd0b94a8ad25a41113e03796b25743c41b38714
dcrayne <dan.crayne@gmail.com>
2017-06-04 09:51:47 -0700

    clean up query string, rearrange layout, and get result count dynamically

--- weatherapp.elm
+++ weatherapp.elm
@@ -51,19 +51,39 @@ update msg model =
       (model, getWeather model.zipCode)
 
     SetZipCode zipCode ->
-      (Model zipCode "" "" "" model.weatherData 0, Cmd.none)
+      (Model zipCode "" "" "" model.weatherData 
+                              model.resultCount, Cmd.none)
 
     NewWeatherData (Ok newWeatherData) ->
-      case decodeResultCount of
+      (Model model.zipCode "" "" "" newWeatherData
+             model.resultCount, Cmd.none)
+           
+    NewWeatherData (Err msg) ->
+      (Model model.zipCode "" "" "" ("Error " ++ toString msg) 0, Cmd.none)
+{--
+    NewWeatherData (Ok newWeatherData) ->
+      (Model model.zipCode "" "" "" newWeatherData 0, SetResultCount)
+        |> andThen decodeResultCount --(model, Cmd.none)
+--}
+
+{--
+    NewWeatherData (Ok newWeatherData) ->
+      case decodeResultCount model.weatherData of
         Err msg ->
           (Model model.zipCode "" "" "" (newWeatherData ++ msg) 0, Cmd.none)
            
         Ok value ->
           (Model model.zipCode "" "" "" newWeatherData value, Cmd.none)
 
-    NewWeatherData (Err _) ->
-      (Model model.zipCode "" "" "" "Error while sending" 0, Cmd.none)
+    NewWeatherData (Err msg) ->
+      (Model model.zipCode "" "" "" ("Error " ++ toString msg) 0, Cmd.none)
+--}    
 
+{--
+    SetResultCount ->
+      (Model model.zipCode "" "" "" model.weatherData 
+             (String.toInt (decodeResultCount model)), Cmd.none)
+--}
 
 
 -- VIEW
@@ -72,14 +92,32 @@ update msg model =
 view : Model -> Html Msg
 view model =
   div []
-    [ h2 []  [text model.zipCode]
-    , input [ type_ "text", placeholder "Enter Zip Code", onInput SetZipCode ] []
+    [ h2 []  [ text model.zipCode ]
+    , input  [ type_ "text", placeholder "Enter Zip Code", 
+                             onInput SetZipCode ] []
     , button [ onClick GetWeather ] [ text "Get Weather" ]
     , br []  []
     , p  []  [ text model.weatherData ]
-    , h3 []  [ text ("Result Count: " ++ toString model.resultCount) ]
+    , updateResultCount model
     ]
 
+
+updateResultCount : Model -> Html Msg
+updateResultCount model =
+  let count = 
+    decodeResultCount model.weatherData
+  in
+    case count of
+      Ok value ->
+        h3 [] [ text ("Results: " ++ toString value) ]
+
+      Err msg ->
+        h3 [] [ text "No Results" ]
+
+
+decodeResultCount : String -> Result String Int
+decodeResultCount json = decodeString (field "metadata" 
+                         (field "resultset" (field "count" int))) json
 
 
 -- SUBSCRIPTIONS
@@ -104,91 +142,8 @@ getWeather zipCode =
     , url = "https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&locationid=ZIP:" ++ zipCode ++ "&startdate=2010-05-01&enddate=2010-05-01"
     , body = Http.emptyBody
     , expect = Http.expectString
---    , expect = expectStringResponse (\_ -> Ok ())
     , timeout = Nothing
     , withCredentials = False
     }
   in
     Http.send NewWeatherData (Http.request weatherRequest)
-
-
-decodeJson : Result String Int
-decodeJson = decodeString (field "metadata" (field "resultset" (field "offset" int))) json
-
---decodeResultCount : Model -> Result String Int
---decodeResultCount model = decodeString (field "metadata" (field "resultset" (field "count" int))) model.weatherData
-
-decodeResultCount : Result String Int
-decodeResultCount = decodeString (field "metadata" (field "resultset" (field "count" int))) json
-
-json : String
-json = """
-{
-  "metadata": {
-    "resultset": {
-      "offset": 1,
-      "count": 8,
-      "limit": 25
-    }
-  },
-
-  "results": [
-    {
-      "date": "2010-05-01T00:00:00",
-      "datatype": "PRCP",
-      "station": "GHCND:US1NCBC0005",
-      "attributes": ",,N,",
-      "value": 0
-    },
-    {
-      "date": "2010-05-01T00:00:00",
-      "datatype": "SNOW",
-      "station": "GHCND:US1NCBC0005",
-      "attributes": ",,N,",
-      "value": 0
-    },
-    {
-      "date": "2010-05-01T00:00:00",
-      "datatype": "PRCP",
-      "station": "GHCND:USW00013872",
-      "attributes": ",,0,2400",
-      "value": 3
-    },
-    {
-      "date": "2010-05-01T00:00:00",
-      "datatype": "SNOW",
-      "station": "GHCND:USW00013872",
-      "attributes": ",,0,",
-      "value": 0
-    },
-    {
-      "date": "2010-05-01T00:00:00",
-      "datatype": "SNWD",
-      "station": "GHCND:USW00013872",
-      "attributes": ",,0,",
-      "value": 0
-    },
-    {
-      "date": "2010-05-01T00:00:00",
-      "datatype": "TMAX",
-      "station": "GHCND:USW00013872",
-      "attributes": ",,0,2400",
-      "value": 267
-    },
-    {
-      "date": "2010-05-01T00:00:00",
-      "datatype": "TMIN",
-      "station": "GHCND:USW00013872",
-      "attributes": ",,0,2400",
-      "value": 139
-    },
-    {
-      "date": "2010-05-01T00:00:00",
-      "datatype": "TOBS",
-      "station": "GHCND:USW00013872",
-      "attributes": ",,0,2400",
-      "value": 206
-    }
-  ]
-}
-"""
